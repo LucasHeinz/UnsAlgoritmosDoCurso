98e0ebff0caa1c5e27f9f02a2b2d15021c2d8f5a
noturno <noturno@SALA190.proway.treina>
2017-05-18 21:45:19 -0300

    A maior parte dos exercícios do dia 18/05. (Scanner e uns Algoritmos)

--- algoritmos/src/exercicios/numero3/E.java
+++ algoritmos/src/exercicios/numero3/E.java
@@ -5,7 +5,7 @@ public class E {
 	public static void main(String[] args) {
 		
 		int num1 = 4, num2 = 7, num3 = 10;
-		double num4 = 3;
+		double num4 = 3d;
 		
 		System.out.println((num1+ num2 + num3) / num4);
 	}
